bb81eb05087787c6333b08b1b01c4107e2c322a5
Starship Code <76881333+StarshipCode@users.noreply.github.com>
2021-12-25 23:23:47 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 
 const jokes = require("./jokes.json")
+const port = process.env.PORT || 8000
+
+//Configurations
+app.set("views", path.join(__dirname + "/views"))
 
 app.get("/random_joke", (req, res) =>{
     res.json(jokes[Math.floor(Math.random() * jokes.length)])
 })
-let port = process.env.PORT || 8000
+
+app.get("/", (req, res) => {
+    res.send("index.html")
+})
+
 app.listen(port, e=>{
     console.log("Server running on port "+ port)
 })
