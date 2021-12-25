d07865915426298dc7c6e5fb804adc25ace1835e
Starship Code <76881333+StarshipCode@users.noreply.github.com>
2021-12-25 23:09:01 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ app.get("/random_joke", (req, res) =>{
     res.json(jokes[Math.floor(Math.random() * jokes.length)])
 })
 
-app.listen(7000, e=>{
+app.listen(8000, e=>{
     console.log("Server running")
 })
