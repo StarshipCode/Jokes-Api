0ece37d8469e9dbaea9cb4bfa67388f4cc04e017
Starship Code <76881333+StarshipCode@users.noreply.github.com>
2021-12-25 23:34:52 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ const port = process.env.PORT || 8000
 app.set("view engine","ejs")
 app.set("views", path.join(__dirname + "/views"))
 
+app.use(express.urlencoded({extended:false}))
+
 app.get("/random_joke", (req, res) =>{
     res.json(jokes[Math.floor(Math.random() * jokes.length)])
 })
 
 app.get("/", (req, res) => {
-    res.render("index.html",{joke:jokes[Math.floor(Math.random() * jokes.length)]})
+    res.render("index",{joke:jokes[Math.floor(Math.random() * jokes.length)]})
 })
 
 app.listen(port, e=>{
