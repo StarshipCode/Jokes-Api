c50e6ad60efaa69e56ba0fe74e9704350d2292fd
Starship Code <76881333+StarshipCode@users.noreply.github.com>
2021-12-25 23:16:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const jokes = require("./jokes.json")
 app.get("/random_joke", (req, res) =>{
     res.json(jokes[Math.floor(Math.random() * jokes.length)])
 })
-
-app.listen(process.env.PORT || 8000, e=>{
-    console.log("Server running")
+let port = process.env.PORT || 8000
+app.listen(port, e=>{
+    console.log("Server running on port "+ port)
 })
