92a503cf58562a3aa38272c1967bc7d23a4f6a26
Starship Code <76881333+StarshipCode@users.noreply.github.com>
2021-12-26 01:13:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const ejs = require("ejs")
+//const ejs = require("ejs")
 
 const jokes = require("./jokes.json")
 const port = process.env.PORT || 8000
 
 //Configurations
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 app.set("views", path.join(__dirname + "/views"))
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
-app.get("/random_joke", (req, res) =>{
+app.get("/random_joke", (req, res) => {
     res.json(jokes[Math.floor(Math.random() * jokes.length)])
 })
 
@@ -24,13 +24,12 @@ app.get("/ping", (req, res) =>{
     res.render("pong")
 })
 
-app.get("/plain", (req,res) => {
+app.get("/plain", (req, res) => {
     let joke = jokes[Math.floor(Math.random() * jokes.length)]
     res.setHeader('content-type', 'text/plain');
     res.send(joke.setup + "\n" + joke.punchline + "\n")
 })
 app.get("/plain/:type", (req, res) => {
-    app.get("/plain/:type", (req, res) => {
     let type = req.params.type
     let joke = jokes.filter(e => e.type == type)
     joke = joke[Math.floor(Math.random() * joke.length)]
@@ -43,7 +42,6 @@ app.get("/plain/:type", (req, res) => {
         res.send("Not found this kind of joke"+ "\n")
     }
 })
-})
-app.listen(port, e=>{
-    console.log("Server running on port "+ port)
+app.listen(port, e => {
+    console.log("Server running on port " + port)
 })
